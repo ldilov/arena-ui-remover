5fbef687e8bf5aec6d050031a8767bca5c4cdc17
Lazar Dilov <ldilov@isimarkets.com>
2024-09-10 20:31:35 +0300

    Spam fix

--- ArenaUIRemover.lua
+++ ArenaUIRemover.lua
@@ -6,7 +6,6 @@ local hiddenFrame = CreateFrame("Frame")
 local function handleFrame(self, spec)
     if InCombatLockdown() then return end
     self:SetParent(hiddenFrame)
-    print("|cffFFF569ArenaUIRemover|r loaded!")
 end
 
 eventFrame:SetScript("OnEvent", function(self, event, name)
